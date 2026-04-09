bb4d8907e7170dc38e8637b16cceeb39952d21ba
BIT <42350280+GlaucousGlaucus@users.noreply.github.com.>
2026-04-09 09:46:54 +0530

    9th April 2026

--- Python/19th March/Library/q15.py
+++ Python/19th March/Library/q15.py
@@ -1,5 +1,6 @@
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
 
 data = load_iris()
 X, y = data.data, data.target
@@ -8,3 +9,12 @@ model = DecisionTreeClassifier(criterion="entropy")
 model.fit(X, y)
 
 print(model.predict(X[:5]))
+
+plt.figure(figsize=(12, 8))
+plot_tree(
+    model,
+    feature_names=data.feature_names,
+    class_names=data.target_names,
+    filled=True
+)
+plt.show()
